f0042840c9836f48a1e10e85a4c4f6d963b6691d
Arnaud Kemp <ArnoKP@users.noreply.huggingface.co>
2023-12-12 16:09:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def result():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True)
+    #app.run(
+    #    host='0.0.0.0',
+    #    port=5000,
+    #    debug=True)
