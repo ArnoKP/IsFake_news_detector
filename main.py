c747c38d26a438c6a93ac7d5d11e7faebe08ab79
Arnaud <arnaudkemp@gmail.com>
2023-12-14 15:57:05 +0100

    add

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, Field
 from flask import Flask, render_template, request
 import requests
 import nltk
-#from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize
 #from nltk.corpus import stopwords
 
 #nltk.download('punkt')
