146f2fd8d9e329e90b54c5076e622acd27821ac6
Arnaud <arnaudkemp@gmail.com>
2023-12-14 20:19:58 +0100

    change file

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def token_pad_text(text):
     #                  oov_token = 'UNKN')
 
     tokenizer.fit_on_texts(text)
-    text_vect = tokenizer.texts_to_sequences(tokens)
+    text_vect = tokenizer.texts_to_sequences(text)
     
     text_pad = sequence.pad_sequences(text_vect,
                                   value=0,
